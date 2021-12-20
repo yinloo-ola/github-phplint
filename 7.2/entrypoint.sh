918561ee36e3fb946d5642a5d126fa506532eca9
Tan Yin Loo <91864258+yinloo-ola@users.noreply.github.com>
2021-12-20 18:50:37 +0800

    Update entrypoint.sh

--- 7.2/entrypoint.sh
+++ 7.2/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-sh -c "! (find . -type f -name \"*.php\" $1 -exec php -l -n {} \; | grep -v \"No syntax errors detected\")"
-IFS=' ' read -r -a array <<< $1
+# sh -c "! (find . -type f -name \"*.php\" $1 -exec php -l -n {} \; | grep -v \"No syntax errors detected\")"
+IFS=' ' read -r -a array <<< "$1"
 for element in "${array[@]}"
 do
     php -l -n "$element"
