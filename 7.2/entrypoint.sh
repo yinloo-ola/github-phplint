74ec0f9d42b177026c2ec6ab6395fa4416980768
Tan Yin Loo <91864258+yinloo-ola@users.noreply.github.com>
2021-12-20 19:00:19 +0800

    Update entrypoint.sh

--- 7.2/entrypoint.sh
+++ 7.2/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
 # sh -c "! (find . -type f -name \"*.php\" $1 -exec php -l -n {} \; | grep -v \"No syntax errors detected\")"
-IFS=' ' read -r -a array <<< "$1"
-for element in "${array[@]}"
-do
-    php -l -n "$element"
+echo $1
+export IFS=" "
+sentence=$1
+for file in $sentence; do
+  php -l -n $file
 done
